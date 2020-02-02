d7c9abc3aed650bf6b4942b3e0c85eae45a0c41e
EC2 Default User <ec2-user@ip-172-31-27-10.us-east-2.compute.internal>
2020-02-02 23:49:35 +0000

    new

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.get('/Lab1', function (req, res) {
+  res.send('index.html');
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

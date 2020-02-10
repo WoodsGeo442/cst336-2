afbff889a44a522bdf874436b7070738a62036c3
WoodsGeo442 <44784501+WoodsGeo442@users.noreply.github.com>
2020-02-09 23:47:17 -0800

    Update hw1.js

--- routes/hw1.js
+++ routes/hw1.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 
-router.get('/hw1', function(req, res, next) {
+router.get('/homework1', function(req, res, next) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
